5c45b7ca66be4a8327dd29cf5e04e03be5d42070
WhiteRose7303 <hadarov@tichonet.com>
2020-01-24 18:43:12 +0200

    24/1/20 Added Query

--- 4080 Project/_4080_Project/views.py
+++ 4080 Project/_4080_Project/views.py
@@ -5,18 +5,37 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from _4080_Project import app
-from flask_wtf import FlaskForm 
-from wtforms import StringField, SubmitField 
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
 
 
 app.config['SECRET_KEY'] = 'The first argument to the field'
-
-
 
 
 @app.route('/')
@@ -68,27 +87,36 @@ def Privacy():
     )
 
 class QueryFormStructure(FlaskForm):
-    name = StringField('Country Name?)' , validators = [DataRequired()])
+    name = StringField('Country Name?' , validators = [DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/qurey' , methods = ['GET' , 'POST'])
 def qurey():
     print("running from qurey()")
-    name = ''
+    name = None
     capital = ''
-    form = QueryFormStructure()
-    df = pd.read_csv('capitals.csv')
+    df = pd.read_csv(path.join(path.dirname(__file__) , 'static\\Data\\Capitals.csv'))
     df = df.set_index('Country')
-    if form.validate_on_submit():
+    form = QueryFormStructure(request.form)
+    if (request.method == 'POST' ):
         name = form.name.data
-        if name in df.index:
-            capital = df.loc[name , 'Capital']
+        if (name in df.index):
+            capital = df.loc[name,'Capital']
         else:
-            capital = name + ', no such country'
-        form.name.data = ''
-    return render_template('qurey.html' , form=form, name = capital,
-                           raw_data_table=df.to_html(classes = 'table table-hover'))
+            capital = name+ ', no such country'
+        form.name.data= ''
 
+    raw_data_table = df.to_html(classes = 'table table-hover')
+
+
+    return render_template('qurey.html',
+                           form= form,
+                           name = capital,
+                           raw_data_table = raw_data_table,
+                           title = 'Query by the user',
+                           year=datetime.now().year,
+                           message='query input'
+                           )
 
 
 
--- 4080 Project/runserver.py
+++ 4080 Project/runserver.py
@@ -11,4 +11,7 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+        
+        app.config['SECRET_KEY'] = 'The first argument to the field'
+
     app.run(HOST, PORT)
