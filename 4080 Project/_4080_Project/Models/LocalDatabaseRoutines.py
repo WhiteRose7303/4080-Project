bfcf72dadbb1d3ceaa2d2ca45629ca8a8d203a77
WhiteRose7303 <hadarov@tichonet.com>
2020-02-20 22:34:00 +0200

    Update for the path

--- 4080 Project/_4080_Project/Models/LocalDatabaseRoutines.py
+++ 4080 Project/_4080_Project/Models/LocalDatabaseRoutines.py
@@ -23,8 +23,9 @@ class LocalDatabaseServiceRoutines(object):
     def __init__(self):
         self.name = 'Data base service routines'
         self.index = {}
-        #/home/HadarOva5384/4080-Project/4080 Project/_4080_Project/static/Data/users.csv
+        #'/home/HadarOva5384/4080-Project/4080 Project/_4080_Project/static/Data/users.csv'
         self.UsersDataFile = pd.read_csv(USER)
+        #path.join(path.dirname(__file__), '/home/HadarOva5384/4080-Project/4080 Project/_4080_Project/static/Data/users.csv')
 
 
 # -------------------------------------------------------
